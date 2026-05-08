aebc84de9321ae87fe334621c884a5577066f112
Tushar Desai <imtushardesai@gmail.com>
2026-05-08 16:10:54 +0530

    fevicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Baby Kick Counter - Bumpsy | Fetal Movement & Pregnancy Log",
+  title: " Bumpsy | Fetal Movement & Pregnancy Log",
   description:
     "Baby Kick Counter - Bumpsy helps pregnant mothers track fetal kicks, monitor movement patterns, time contractions, and share doctor-ready reports. Minimal, calm, offline, and built for reassurance.",
+  icons: {
+    icon: '/playstore.png?v=2',
+    apple: '/playstore.png?v=2',
+    shortcut: '/playstore.png?v=2',
+  },
 };
 
 export default function RootLayout({
@@ -14,6 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+          <link rel="icon" type="image/png" sizes="48x48" href="/playstore.png?v=2" />
+          <link rel="shortcut icon" href="/playstore.png?v=2" />
+          <link rel="apple-touch-icon" href="/playstore.png?v=2" />
+        </head>
       <body>{children}</body>
     </html>
   );
